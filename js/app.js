c4eda1728d578cd9ca2d7b6dfb4aae56fce4dd47
Kathy White <kawhite8503@gmail.com>
2022-06-06 08:38:22 -0500

    push entire project at once

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log('sanity check')
+//cashed element references
+
+
+
+const form = document.querySelector('form')
+const textBox = document.querySelector('#text-box')
+const submitButton = document.querySelector('#submit-button')
+const todoList = document.querySelector('#todo-list')
+const resetBtn = document.querySelector('#reset-button')
+
+resetBtn.setAttribute('hidden', true)
+
+submitButton.addEventListener('click', function(evt){
+  evt.preventDefault()
+  init()
+})
+resetBtn.addEventListener('click', function(evt){
+  resetList()
+})
+
+
+
+function resetList() {
+  todoList.remove()
+}
+//creates new list item
+function init(){
+  const li = document.createElement('li')
+  li.textContent = textBox.value
+  while (textBox.value !== "") {
+  todoList.appendChild(li)
+  textBox.value = ''
+  resetBtn.removeAttribute('hidden', false)
+}
+}
